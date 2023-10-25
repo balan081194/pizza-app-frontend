ee5954f024992bdc7d3d368c1ccd2f7f7ad79257
balan081194 <123051259+balan081194@users.noreply.github.com>
2023-10-25 13:05:10 +0530

    Update pizzaActions.js api

--- src/actions/pizzaActions.js
+++ src/actions/pizzaActions.js
@@ -5,7 +5,7 @@ export const getAllPizzas = () => async dispatch => {
 
    dispatch({ type: 'GET_PIZZAS_REQUEST' })
 
-   const response =  await axios.get('/api/pizzas/getallpizzas').then((response) => {
+   const response =  await axios.get('https://pizza-app-backend-five.vercel.app/api/pizzas/getallpizzas').then((response) => {
       console.log(response)
       dispatch({ type: 'GET_PIZZAS_SUCCESS', payload: response.data })
    }).catch((err) => {
@@ -15,7 +15,7 @@ export const getAllPizzas = () => async dispatch => {
 }
 
    // try {
-   //      const response = await axios.get('/api/pizzas/getallpizzas')
+   //      const response = await axios.get('https://pizza-app-backend-five.vercel.app//api/pizzas/getallpizzas')
    //      console.log(response)
    //      dispatch({type:'GET_PIZZAS_SUCCESS', payload:response.data})
 
